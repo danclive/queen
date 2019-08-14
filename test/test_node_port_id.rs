d6d64ddf7d0cee490cf2ae2f1e61c09a916136d6
danc <dangcheng@hotmail.com>
2019-08-14 17:32:38 +0800

    test

---
+++ test/test_node_port_id.rs
@@ -0,0 +1,222 @@
+use std::thread;
+use std::net::TcpStream;
+use std::time::Duration;
+use std::io::ErrorKind::WouldBlock;
+
+use queen::{Node, node::NodeConfig};
+use queen::nson::{msg, Message};
+use queen::error::ErrorCode;
+use queen::util::{write_socket, read_socket};
+
+use super::get_free_addr;
+
+#[test]
+fn duplicate() {
+    let addr = get_free_addr();
+
+    let addr2 = addr.clone();
+    thread::spawn(move || {
+        let mut config = NodeConfig::new();
+
+        config.add_tcp(addr2).unwrap();
+        config.set_hmac_key("queen");
+
+        let mut node = Node::bind(config, ()).unwrap();
+
+        node.run().unwrap();
+    });
+
+    thread::sleep(Duration::from_secs(1));
+
+    // client 1
+    let mut socket = TcpStream::connect(&addr).unwrap();
+
+    // client2
+    let mut socket2 = TcpStream::connect(&addr).unwrap();
+
+    // client3
+    let mut socket3 = TcpStream::connect(addr).unwrap();
+
+    // client 1 auth
+    let msg = msg!{
+        "_chan": "_auth",
+        "username": "aaa",
+        "password": "bbb",
+        "_ptid": "id123"
+    };
+
+    write_socket(&mut socket, b"queen", msg.to_vec().unwrap()).unwrap();
+
+    let data = read_socket(&mut socket, b"queen").unwrap();
+    let recv = Message::from_slice(&data).unwrap();
+    assert!(recv.get_i32("ok").unwrap() == 0);
+
+    // client 2 auth
+    let msg = msg!{
+        "_chan": "_auth",
+        "username": "aaa",
+        "password": "bbb",
+        "_ptid": "id456"
+    };
+
+    write_socket(&mut socket2, b"queen", msg.to_vec().unwrap()).unwrap();
+
+    let data = read_socket(&mut socket2, b"queen").unwrap();
+    let recv = Message::from_slice(&data).unwrap();
+    assert!(recv.get_i32("ok").unwrap() == 0);
+
+    // client 3 auth
+    let msg = msg!{
+        "_chan": "_auth",
+        "username": "aaa",
+        "password": "bbb",
+        "_ptid": "id456"
+    };
+
+    write_socket(&mut socket3, b"queen", msg.to_vec().unwrap()).unwrap();
+
+    let data = read_socket(&mut socket3, b"queen").unwrap();
+    let recv = Message::from_slice(&data).unwrap();
+    assert!(ErrorCode::has_error(&recv) == Some(ErrorCode::DuplicatePortId));
+}
+
+#[test]
+fn port_to_port() {
+    let addr = get_free_addr();
+
+    let addr2 = addr.clone();
+    thread::spawn(move || {
+        let mut config = NodeConfig::new();
+
+        config.add_tcp(addr2).unwrap();
+        config.set_hmac_key("queen");
+
+        let mut node = Node::bind(config, ()).unwrap();
+
+        node.run().unwrap();
+    });
+
+    thread::sleep(Duration::from_secs(1));
+
+    // client 1
+    let mut socket = TcpStream::connect(&addr).unwrap();
+
+    // client2
+    let mut socket2 = TcpStream::connect(&addr).unwrap();
+
+    // client3
+    let mut socket3 = TcpStream::connect(addr).unwrap();
+
+    // client 1 auth
+    let msg = msg!{
+        "_chan": "_auth",
+        "username": "aaa",
+        "password": "bbb",
+        "_ptid": "id123"
+    };
+
+    write_socket(&mut socket, b"queen", msg.to_vec().unwrap()).unwrap();
+
+    let data = read_socket(&mut socket, b"queen").unwrap();
+    let recv = Message::from_slice(&data).unwrap();
+    assert!(recv.get_i32("ok").unwrap() == 0);
+
+    // client 2 auth
+    let msg = msg!{
+        "_chan": "_auth",
+        "username": "aaa",
+        "password": "bbb",
+        "_ptid": "id456"
+    };
+
+    write_socket(&mut socket2, b"queen", msg.to_vec().unwrap()).unwrap();
+
+    let data = read_socket(&mut socket2, b"queen").unwrap();
+    let recv = Message::from_slice(&data).unwrap();
+    assert!(recv.get_i32("ok").unwrap() == 0);
+
+    // client 3 auth
+    let msg = msg!{
+        "_chan": "_auth",
+        "username": "aaa",
+        "password": "bbb",
+        "_ptid": "id789"
+    };
+
+    write_socket(&mut socket3, b"queen", msg.to_vec().unwrap()).unwrap();
+
+    let data = read_socket(&mut socket3, b"queen").unwrap();
+    let recv = Message::from_slice(&data).unwrap();
+    assert!(recv.get_i32("ok").unwrap() == 0);
+
+    // client 1 attach
+    let msg = msg!{
+        "_chan": "_atta",
+        "_valu": "aaa"
+    };
+
+    write_socket(&mut socket, b"queen", msg.to_vec().unwrap()).unwrap();
+
+    let data = read_socket(&mut socket, b"queen").unwrap();
+    let recv = Message::from_slice(&data).unwrap();
+    assert!(recv.get_i32("ok").unwrap() == 0);
+
+    // client 2 attach
+    let msg = msg!{
+        "_chan": "_atta",
+        "_valu": "aaa"
+    };
+
+    write_socket(&mut socket2, b"queen", msg.to_vec().unwrap()).unwrap();
+
+    let data = read_socket(&mut socket2, b"queen").unwrap();
+    let recv = Message::from_slice(&data).unwrap();
+    assert!(recv.get_i32("ok").unwrap() == 0);
+
+    // client 3 try send
+    let msg = msg!{
+        "_chan": "aaa",
+        "hello": "world",
+        "_ack": 123,
+        "_to": "aaa"
+    };
+
+    write_socket(&mut socket2, b"queen", msg.to_vec().unwrap()).unwrap();
+
+    let data = read_socket(&mut socket2, b"queen").unwrap();
+    let recv = Message::from_slice(&data).unwrap();
+    assert!(ErrorCode::has_error(&recv) == Some(ErrorCode::TargetPortIdNotExist));
+
+    // client 3 try send
+    let msg = msg!{
+        "_chan": "aaa",
+        "hello": "world",
+        "_ack": 123,
+        "_to": "id123"
+    };
+
+    write_socket(&mut socket2, b"queen", msg.to_vec().unwrap()).unwrap();
+
+    let data = read_socket(&mut socket2, b"queen").unwrap();
+    let recv = Message::from_slice(&data).unwrap();
+    assert!(recv.get_i32("ok").unwrap() == 0);
+
+    // client 1 tyy recv
+    let data = read_socket(&mut socket, b"queen").unwrap();
+    let recv = Message::from_slice(&data).unwrap();
+    assert!(recv.get_str("hello").unwrap() == "world");
+
+    //client 2 try recv
+    socket2.set_read_timeout(Some(Duration::from_secs(1))).unwrap();
+    let recv = read_socket(&mut socket2, b"queen");
+    match recv {
+       Ok(recv) => panic!("{:?}", recv),
+       Err(err) => {
+            if let WouldBlock = err.kind() {
+                // pass
+            } else {
+                panic!("{:?}", err);
+            }        
+       }
+    }
+}
