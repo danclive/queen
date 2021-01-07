e5d25b4f54279fe012559f47bd49472ff2f0d608
danc <dangcheng@hotmail.com>
2021-01-07 21:50:56 +0800

    rewrite network read

--- src/net/network.rs
+++ src/net/network.rs
@@ -2,9 +2,12 @@ use std::time::Instant;
 use std::collections::VecDeque;
 use std::time::Duration;
 use std::io::{
+    self,
     Write,
-    ErrorKind::{WouldBlock, Interrupted}
+    Read,
+    ErrorKind::{WouldBlock, Interrupted, InvalidData, BrokenPipe}
 };
+use std::mem;
 
 use queen_io::{
     epoll::{Epoll, Event, Events, Token, Ready, EpollOpt},
@@ -18,10 +21,10 @@ use nson::{Message, msg};
 
 use crate::Wire;
 use crate::crypto::Crypto;
-use crate::util::message::read_nonblock;
 use crate::error::{Error, Result, RecvError, Code};
 use crate::dict::*;
 use crate::timer::wheel::Wheel;
+use crate::MAX_MESSAGE_LEN;
 
 use super::Codec;
 use super::KeepAlive;
@@ -118,7 +121,7 @@ impl<C: Codec> NetWork<C> {
                                         &wire,
                                         Token(token),
                                         Ready::readable(),
-                                        EpollOpt::level()
+                                        EpollOpt::edge()
                                     )?;
 
                                     self.epoll.add(
@@ -171,7 +174,8 @@ impl<C: Codec> NetWork<C> {
                                                 CHAN: KEEP_ALIVE
                                             };
 
-                                            net_conn.push_data(&self.epoll, message)?;
+                                            net_conn.push_data(message)?;
+                                            net_conn.want_write(&self.epoll)?;
                                         }
                                     } else {
                                         self.remove_conn(index)?;
@@ -201,25 +205,10 @@ impl<C: Codec> NetWork<C> {
     }
 
     fn dispatch_wire(&mut self, index: usize) -> Result<()> {
-        let mut remove = false;
-
-        if let Some(wire) = self.wires.get(index) {
-            match wire.recv() {
-                Ok(message) => {
-                    if let Some(net_conn) = self.nets.get_mut(index) {
-                        net_conn.push_data(&self.epoll, message)?;
-                    }
-                }
-                Err(err) => {
-                    if !matches!(err, RecvError::Empty) {
-                        remove = true
-                    }
-                }
+        if let Some(_wire) = self.wires.get(index) {
+            if let Some(net_conn) = self.nets.get_mut(index) {
+                net_conn.want_write(&self.epoll)?;
             }
-        }
-
-        if remove {
-            self.remove_conn(index)?;
         }
 
         Ok(())
@@ -239,11 +228,37 @@ impl<C: Codec> NetWork<C> {
         }
 
         if ready.is_writable() {
-            if let Some(net_conn) = self.nets.get_mut(index) {
-                let ret = net_conn.write();
-                if ret.is_err() {
-                    log::debug!("net_conn.write: {:?}", ret);
-                    remove = true;
+            if let Some(wire) = self.wires.get(index) {
+                if let Some(net_conn) = self.nets.get_mut(index) {
+
+                    if !net_conn.w_buffer.is_empty() {
+                        let ret = net_conn.write(&self.epoll);
+                        if ret.is_err() {
+                            log::debug!("net_conn.write: {:?}", ret);
+                            remove = true;
+                        }
+                    } else {
+                        match wire.recv() {
+                            Ok(message) => {
+                                net_conn.push_data(message)?;
+
+                                let ret = net_conn.write(&self.epoll);
+                                if ret.is_err() {
+                                    log::debug!("net_conn.write: {:?}", ret);
+                                    remove = true;
+                                }
+                            }
+                            Err(err) => {
+                                if matches!(err, RecvError::Empty) {
+                                    if net_conn.w_buffer.is_empty() {
+                                        net_conn.interest.remove(Ready::writable());
+                                    }
+                                } else {
+                                    remove = true
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -270,8 +285,8 @@ struct NetConn<C: Codec> {
     token: usize,
     stream: TcpStream,
     interest: Ready,
-    r_buffer: Vec<u8>,
-    w_buffer: VecDeque<Vec<u8>>,
+    r_buffer: (usize, Vec<u8>), // offset, buffer
+    w_buffer: VecDeque<(usize, Vec<u8>)>, // offset, buffer
     codec: C,
     crypto: Option<Crypto>,
     time_id: usize,
@@ -286,7 +301,7 @@ impl<C: Codec> NetConn<C> {
             token,
             stream,
             interest: Ready::readable() | Ready::hup() | Ready::error(),
-            r_buffer: Vec::with_capacity(1024),
+            r_buffer: (0, Vec::new()),
             w_buffer: VecDeque::new(),
             codec,
             crypto,
@@ -296,22 +311,24 @@ impl<C: Codec> NetConn<C> {
     }
 
     fn read(&mut self, epoll: &Epoll, wire: &Wire<Message>, now: Instant) -> Result<()> {
+        self.keep_alive.reset(now);
+
         loop {
-            let ret = read_nonblock(&mut self.stream, &mut self.r_buffer);
+            let ret = read(&mut self.stream, &mut self.r_buffer);
 
             match ret {
                 Ok(ret) => {
                     if let Some(bytes) = ret {
                         let mut message = self.codec.decode(&self.crypto, bytes)?;
 
-                        self.keep_alive.reset(now);
-
                         if message.get_str(CHAN) == Ok(KEEP_ALIVE) {
                             log::debug!("recv keep alive message, addr: {:?}", self.stream.peer_addr()?);
 
                             if Code::get(&message).is_none() {
                                 Code::Ok.set(&mut message);
-                                self.push_data(epoll, message)?;
+
+                                self.push_data(message)?;
+                                self.want_write(epoll)?;
                             }
 
                             continue
@@ -335,17 +352,16 @@ impl<C: Codec> NetConn<C> {
         Ok(())
     }
 
-    fn write(&mut self) -> Result<()> {
-        while let Some(front) = self.w_buffer.front_mut() {
-            match self.stream.write(front) {
+    fn write(&mut self, epoll: &Epoll) -> Result<()> {
+        while let Some((index, front)) = self.w_buffer.front_mut() {
+            match self.stream.write(&front[*index..]) {
                 Ok(size) => {
                     if size == 0 {
                         return Err(Error::BrokenPipe("NetConn.write".to_string()))
-                    } else if size >= front.len() {
+                    } else if size >= front.len() - *index {
                         self.w_buffer.pop_front();
-                    } else if size < front.len() {
-                        // assert!(size > front.len());
-                        *front = front[size..].to_vec();
+                    } else {
+                        *index += size;
                     }
                 }
                 Err(err) => {
@@ -361,20 +377,24 @@ impl<C: Codec> NetConn<C> {
         }
 
         if self.w_buffer.is_empty() {
-            self.interest.remove(Ready::writable());
-
             if self.w_buffer.capacity() > 64 {
                 self.w_buffer.shrink_to_fit();
             }
         }
 
+        if self.interest.contains(Ready::writable()) {
+            epoll.modify(
+                &self.stream,
+                Token(self.token),
+                self.interest,
+                EpollOpt::edge()
+            )?;
+        }
+
         Ok(())
     }
 
-    fn push_data(&mut self, epoll: &Epoll, message: Message) -> Result<()> {
-        let bytes = self.codec.encode(&self.crypto, message)?;
-        self.w_buffer.push_back(bytes);
-
+    fn want_write(&mut self, epoll: &Epoll) -> Result<()> {
         if !self.interest.contains(Ready::writable()) {
             self.interest.insert(Ready::writable());
 
@@ -388,4 +408,101 @@ impl<C: Codec> NetConn<C> {
 
         Ok(())
     }
+
+    fn push_data(&mut self, message: Message) -> Result<()> {
+        let bytes = self.codec.encode(&self.crypto, message)?;
+        self.w_buffer.push_back((0, bytes));
+
+        Ok(())
+    }
+}
+
+fn read(stream: &mut TcpStream, buffer: &mut (usize, Vec<u8>)) -> io::Result<Option<Vec<u8>>> {
+    if buffer.1.is_empty() {
+        let mut len_bytes = [0u8; 4];
+        let size = stream.peek(&mut len_bytes)?;
+
+        if size == 0 {
+            return Err(io::Error::new(BrokenPipe, "BrokenPipe"))
+        } else if size < 4 {
+            let size = stream.read(&mut len_bytes[..size])?;
+            if size == 0 {
+                return Err(io::Error::new(BrokenPipe, "BrokenPipe"))
+            }
+
+            buffer.0 = size;
+            buffer.1 = len_bytes.to_vec();
+
+            return Ok(None)
+        }
+
+        let len = u32::from_le_bytes(len_bytes) as usize;
+        if len < 5 || len > MAX_MESSAGE_LEN {
+            return Err(io::Error::new(InvalidData, format!("Invalid length of {}", len)))
+        }
+
+        // let mut buf = vec![0u8; len];
+        let mut buf = Vec::with_capacity(len);
+        unsafe { buf.set_len(len); }
+
+        let size = stream.read(&mut buf)?;
+
+        if size == 0 {
+            return Err(io::Error::new(BrokenPipe, "BrokenPipe"))
+        } else if size < len {
+            buffer.0 = size;
+            buffer.1 = buf;
+
+            return Ok(None)
+        }
+
+        // assert!(size == len);
+        return Ok(Some(buf))
+    } else {
+        if buffer.0 < 4 {
+            let size = stream.read(&mut buffer.1[buffer.0..])?;
+            if size == 0 {
+                return Err(io::Error::new(BrokenPipe, "BrokenPipe"))
+            }
+
+            buffer.0 += size;
+
+            if buffer.0 == 4 {
+                let len = read_u32(&buffer.1, 0);
+                if len < 5 || len > MAX_MESSAGE_LEN {
+                    return Err(io::Error::new(InvalidData, format!("Invalid length of {}", len)))
+                }
+
+                buffer.0 = 4;
+                buffer.1.reserve(len - 4);
+                unsafe { buffer.1.set_len(len); }
+            }
+        } else {
+            let size = stream.read(&mut buffer.1[buffer.0..])?;
+            if size == 0 {
+                return Err(io::Error::new(BrokenPipe, "BrokenPipe"))
+            }
+
+            buffer.0 += size;
+
+            if buffer.0 == buffer.1.len() {
+                let mut vec = Vec::new();
+                mem::swap(&mut buffer.1, &mut vec);
+                buffer.0 = 0;
+
+                return Ok(Some(vec))
+            }
+        }
+    }
+
+    return Ok(None)
+}
+
+fn read_u32(buf: &[u8], start: usize) -> usize {
+    (
+        u32::from(buf[start]) |
+        u32::from(buf[start + 1]) << 8 |
+        u32::from(buf[start + 2]) << 16 |
+        u32::from(buf[start + 3]) << 24
+    ) as usize
 }
